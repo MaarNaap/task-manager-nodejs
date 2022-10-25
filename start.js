7062680feb985d3458c48fc8541aa0a8f11e1543
Belal Abdoh <belal7md@gmail.com>
2022-10-25 20:44:14 +0200

    1

---
+++ start.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+async function startApp(dbConnection, expressApp) {
+    const port = process.env.port || 8521;
+    try {
+        await dbConnection(process.env.MONGO_URL);
+        expressApp.listen(port, console.log(`server on ${port}...`));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+module.exports = startApp;
